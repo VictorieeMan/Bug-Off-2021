9dadb89b4f36e57f39954c516b40934ebbc2498e
VictorieeMan <59509525+VictorieeMan@users.noreply.github.com>
2021-11-07 18:12:15 +0100

    Created an experimental flying mob level

---
+++ Assets/Press_Start_2P/level_x.gd
@@ -0,0 +1,50 @@
+extends Node
+
+export (PackedScene) var mob_scorpionfly
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	new_game()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_player_Area2D_hit():
+	pass # Replace with function body.
+
+func game_over():
+	$mobTimer.stop()
+	
+func new_game():
+	$player_Area2D.start($startPosition.position)
+	$startTimer.start()
+
+func _on_startTimer_timeout():
+	$mobTimer.start()
+
+
+func _on_mobTimer_timeout():
+	#Random spawn position on path
+	$mobPath/mobSpawnLocation.offset = randi()
+	#Creating a mob instance
+	var mob = mob_scorpionfly.instance()
+	add_child(mob)
+	#Seting direction of mob
+	var direction = $mobPath/mobSpawnLocation.rotation + PI / 2
+	#Set a random start location
+	mob.position = $mobPath/mobSpawnLocation.position
+	#Add some randomness to the direction
+	direction += rand_range(-PI/4 , PI/4)
+	mob.rotation=direction
+	#Set the velocity (Speed and direction vector)
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
